484735aa92a5df8e940b4cbb986d601f8127a762
sserva1 <77353742+sserva1@users.noreply.github.com>
2021-04-18 13:34:11 -0500

    Update photomap.js

--- photomap.js
+++ photomap.js
@@ -20,12 +20,9 @@ function createPhotoMap () {
   var map = L.map('map').setView([34.47, -94.24], 6);
   
   // create basemap object. See examples at https://leaflet-extras.github.io/leaflet-providers/preview/
-  var Stamen_Watercolor = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.{ext}', {
-	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-	subdomains: 'abcd',
-	minZoom: 1,
-	maxZoom: 16,
-	ext: 'jpg'
+ var basemap = L.tileLayer('https://basemap.nationalmap.gov/arcgis/rest/services/USGSImageryTopo/MapServer/tile/{z}/{y}/{x}', {
+ maxZoom: 16,
+ attribution: 'USGS'
 }).addTo(map);
 
   
